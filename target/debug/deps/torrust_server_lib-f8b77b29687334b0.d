77819861c145adb1f855f52762263a4425d143f0
Jose Celano <josecelano@gmail.com>
2026-06-19 08:16:43 +0100

    Initial commit: torrust-server-lib v0.1.0

---
+++ target/debug/deps/torrust_server_lib-f8b77b29687334b0.d
@@ -0,0 +1,12 @@
+/home/josecelano/Documents/git/committer/me/github/torrust/torrust-server-lib/target/debug/deps/torrust_server_lib-f8b77b29687334b0.d: src/lib.rs src/logging.rs src/registar.rs src/signals.rs Cargo.toml
+
+/home/josecelano/Documents/git/committer/me/github/torrust/torrust-server-lib/target/debug/deps/libtorrust_server_lib-f8b77b29687334b0.rmeta: src/lib.rs src/logging.rs src/registar.rs src/signals.rs Cargo.toml
+
+src/lib.rs:
+src/logging.rs:
+src/registar.rs:
+src/signals.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
